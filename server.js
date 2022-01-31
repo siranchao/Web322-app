ca0cdad47764c1574683f9b29cbb6366ed7c7356
Siranchao <scao34@myseneca.ca>
2022-01-31 18:53:27 -0500

    update about.html

---
+++ server.js
@@ -0,0 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ______________________ Student ID: ______________ Date: ________________
+*
+*  Online (Heroku) URL: ________________________________________________________
+*
+*  GitHub Repository URL: ______________________________________________________
+*
+********************************************************************************/ 
+
+
+

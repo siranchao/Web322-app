4fe8c36338888ba17510b2b0816b6db048ac8a0c
Siranchao <scao34@myseneca.ca>
2022-02-03 18:14:08 -0500

    complete server.js

---
+++ blog-service.js
@@ -0,0 +1,63 @@
+const fs = require("fs")
+const path = require("path")
+
+let posts = []
+let categories = []
+
+//export functions
+
+module.exports.initialize = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, "/data/posts.json"), "utf-8", (err, data) => {
+            if(err) {
+                console.log(`read posts file error: ${err}`);
+                reject(`Unable to read posts.json file`)
+            } else {
+                posts = JSON.parse(data)
+
+                //read the 2nd file
+                fs.readFile(path.join(__dirname, "/data/categories.json"), "utf-8", (err, data) => {
+                    if(err) {
+                        console.log(`read categories file error: ${err}`);
+                        reject(`Unable to read categories.json file`)
+                    } else {
+                        categories = JSON.parse(data)
+                        resolve("data read successfully");
+                    }
+                })
+            }
+        })
+    })
+}
+
+module.exports.getAllPosts = () => {
+    return new Promise((resolve, reject) => {
+        if(posts.length == 0) {
+            reject(`no results returned`)
+        } else {
+            resolve(posts)
+        }
+    })
+}
+
+module.exports.getPublishedPosts = () => {
+    return new Promise((resolve, reject) => {
+        if(posts.length == 0) {
+            reject(`no results returned`)
+        } else {
+            resolve(posts.filter(ele => 
+                ele.published == true
+            ))
+        }
+    })
+}
+
+module.exports.getCategories = () => {
+    return new Promise((resolve, reject) => {
+        if(categories.length == 0) {
+            reject(`no results returned`)
+        } else {
+            resolve(categories)
+        }
+    })
+}

1757590f96c49d94683c585e6717f803c7b4ea5d
Siranchao <scao34@myseneca.ca>
2022-02-03 18:16:50 -0500

    update v1.0

--- server.js
+++ server.js
@@ -11,5 +11,75 @@
 *
 ********************************************************************************/ 
 
+//imports external modules 
+const express = require("express")
+const path = require("path")
+const blogService = require("./blog-service")
+
+//global constants
+const HTTP_PORT = process.env.port || 8080
+const app = express()
+const HTTPstart = () => {
+    console.log(`Express http server is listening on port: ${HTTP_PORT}`);
+}
+
+//listening on port
+blogService.initialize()
+.then((msg) => {
+    console.log(`server start: ${msg}`);
+    app.listen(HTTP_PORT, HTTPstart)
+})
+.catch((err) => {
+    console.log(err);
+})
 
 
+//serving static file and middle-ware
+app.use(express.static("public"))
+
+
+//setting routes
+app.get("/", (req, res) => {
+    res.redirect("/about")
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"))
+})
+
+app.get("/blog", (req, res) => {
+    blogService.getPublishedPosts()
+    .then((data) => {
+        res.json(data)
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send(`Error Message: ${err}`)
+    })
+})
+
+app.get("/posts", (req, res) => {
+    blogService.getAllPosts()
+    .then((data) => {
+        res.json(data)
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send(`Error Message: ${err}`)
+    })
+})
+
+app.get("/categories", (req, res) => {
+    blogService.getCategories()
+    .then((data) => {
+        res.json(data)
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send(`Error Message: ${err}`)
+    })
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "/views/404.html"))
+})
